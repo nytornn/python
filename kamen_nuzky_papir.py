649dcdacf42e2375ded9704ef76853e37c0103a5
nytornn <76937551+nytornn@users.noreply.github.com>
2022-02-21 18:57:45 +0100

    Add files via upload

---
+++ kamen_nuzky_papir.py
@@ -0,0 +1,63 @@
+import random
+import time
+
+pocitacVyhra = 0
+hracVyhra = 0
+remiza = 0
+konec = 0
+
+while True:
+
+    volby = ["kamen", "papir", "nuzky"]
+
+    hracVolba = input("kamen, nuzky, papir, or konec\n")
+
+    pocitacVolba = (random.choice(volby))
+    print(pocitacVolba)
+
+    if hracVolba == pocitacVolba:
+        print("remiza!\n")
+        remiza += 1
+        konec += 1
+
+    elif hracVolba == "kamen":
+        if pocitacVolba == "papir":
+            print("Pocitac vyhral!\n")
+            pocitacVyhra +=1
+            konec += 1
+
+        else:
+            print("Hrac vyhral!\n")
+            hracVyhra += 1
+            konec += 1
+
+    elif hracVolba == "papir":
+        if pocitacVolba == "kamen":
+            print("Hrac vyhral!\n")
+            hracVyhra += 1
+            konec += 1
+
+        else:
+            print("Pocitac vyhral!\n")
+            pocitacVyhra += 1
+            konec += 1
+
+    elif hracVolba == "nuzky":
+        if pocitacVolba == "kamen":
+            print("Pocitac vyhral!\n")
+            pocitacVyhra += 1
+            konec += 1
+
+        else:
+            print("Hrac vyhral!\n")
+            hracVyhra += 1
+            konec += 1
+
+    elif hracVolba == "konec":
+            volby.append("konec")
+            print("\nKonec Hry!\n")
+            print("Pocitac: ", pocitacVyhra, "vyher!")
+            print("Hrac: ", hracVyhra, "vyher!")
+            print("remiz: ", remiza, "remiza!")
+            time.sleep(2)
+            break
